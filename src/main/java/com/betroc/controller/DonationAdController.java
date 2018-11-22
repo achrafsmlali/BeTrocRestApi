d9e4e9203b37564c3cd90dead5dce3bd111c1a6a
Smlali Achraf <smlali.achraf@gmail.com>
2018-11-22 15:45:04 +0100

    [feat] add post get update on DonationResquest 	- create a DonationRequest controller 	- add uploads repositorywitch contain images loaded by users to gitignore

--- src/main/java/com/betroc/controller/DonationAdController.java
+++ src/main/java/com/betroc/controller/DonationAdController.java
@@ -1,17 +1,10 @@
 package com.betroc.controller;
 
 import com.betroc.model.DonationAd;
-import com.betroc.payload.ApiResponse;
 import com.betroc.repository.DonationAdRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/donationAds")
--- src/main/java/com/betroc/controller/ExchangesAdController.java
+++ src/main/java/com/betroc/controller/ExchangesAdController.java
@@ -2,7 +2,6 @@ package com.betroc.controller;
 
 import com.betroc.model.ExchangeAd;
 import com.betroc.repository.ExchangeAdRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
