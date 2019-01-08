b5122618b07412cb4e3255d1cabb58f42d40f173
DjarallahBrahim <djarallah.brahim@gmail.com>
2019-01-08 11:34:04 +0100

    [update] Get user information with his ads 	- Add ProfileResponse 	- Add findAllByUser for all Ad repositories

---
+++ src/main/java/com/betroc/payload/ProfileResponse.java
@@ -0,0 +1,70 @@
+package com.betroc.payload;
+
+import com.betroc.model.DonationAd;
+import com.betroc.model.DonationRequestAd;
+import com.betroc.model.ExchangeAd;
+
+import java.util.List;
+
+public class ProfileResponse {
+
+    private String username;
+
+    private String email;
+
+    private List<ExchangeAd> exchangeAds;
+
+    private List<DonationAd> donationAds;
+
+    private List<DonationRequestAd> donationRequestAds;
+
+    private int nb_annonce;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<ExchangeAd> getExchangeAds() {
+        return exchangeAds;
+    }
+
+    public void setExchangeAds(List<ExchangeAd> exchangeAds) {
+        this.exchangeAds = exchangeAds;
+    }
+
+    public List<DonationAd> getDonationAds() {
+        return donationAds;
+    }
+
+    public void setDonationAds(List<DonationAd> donationAds) {
+        this.donationAds = donationAds;
+    }
+
+    public List<DonationRequestAd> getDonationRequestAds() {
+        return donationRequestAds;
+    }
+
+    public void setDonationRequestAds(List<DonationRequestAd> donationRequestAds) {
+        this.donationRequestAds = donationRequestAds;
+    }
+
+    public int getNb_annonce() {
+        return nb_annonce;
+    }
+
+    public void setNb_annonce(int nb_annonce) {
+        this.nb_annonce = nb_annonce;
+    }
+}
--- src/main/java/com/betroc/repository/DonationAdRepository.java
+++ src/main/java/com/betroc/repository/DonationAdRepository.java
@@ -1,9 +1,15 @@
 package com.betroc.repository;
 
 import com.betroc.model.DonationAd;
+import com.betroc.model.User;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Transactional
 public interface DonationAdRepository extends AdvertisementBaseRepository<DonationAd> {
+
+    List<DonationAd> findAllByUser(Optional<User> usr);
+
 }
