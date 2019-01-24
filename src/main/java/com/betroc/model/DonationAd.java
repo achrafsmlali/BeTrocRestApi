351fe9ff747f276c8dbbcff23500b8f1c1642666
achrafsmlali <smlali.achraf@gmail.com>
2019-01-24 15:39:07 +0100

    [fix] add getter and setters to lat and long attributs

--- src/main/java/com/betroc/model/DonationAd.java
+++ src/main/java/com/betroc/model/DonationAd.java
@@ -32,4 +32,20 @@ public class DonationAd extends Advertisement{
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
